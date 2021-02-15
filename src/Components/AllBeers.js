9b47660b59e4adf99189ab41f93562006393312f
Stephanie Mitchell <stephsorandom@gmail.com>
2021-02-15 16:54:54 -0500

    not working: :

--- src/Components/AllBeers.js
+++ src/Components/AllBeers.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom'
+import axios from 'axios';
+
 
 function AllBeers(props) {
+    const [showAll, setShowAll] = useState('')
+
+    // const randBeer = Math.floor(Math.random() * 255);
+
+
+    useEffect (() => {
+        axios.get('https://ih-beers-api2.herokuapp.com/beers')
+        .then((res) => {
+            console.log(res)
+            setShowAll(res.data);
+        })
+    })
+    const showbeers= () => {
+        return showAll.map((eachbeer) => {
+            return (
+                <li>
+          <div>
+            <Link to={`/allbeers/${eachbeer._id}`}>{eachbeer.name} </Link>{''}
+            <img src={eachbeer.image_url} alt='/'/>
+          </div>
+        </li>
+            )
+        })
+    }
     return (
         <div>
-            
+            {showbeers()}
         </div>
     );
 }
--- src/Components/RandomBeer.js
+++ src/Components/RandomBeer.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function RandomBeer(props) {
+    const [beer, setBeer] = useState({})
+
+    useEffect(() => {
+        axios.get('https://ih-beers-api2.herokuapp.com/beers/random')
+        .then(res =>{
+            console.log(res)
+            setBeer(res.data)
+    })
+
+
     return (
         <div>
-            
+            <img src={beer.image_url}/>,{beer.name}
         </div>
     );
 }
